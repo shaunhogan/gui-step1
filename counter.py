700d19c1ad4ec1a3dcb69683e825f6dc6ab5e098
Caleb James Smith <colorado.caleb.smith@gmail.com>
2016-10-31 16:55:10 +0100

    counters now know more about gpio output mode, reseting counters and enabling counters. caleb is learning slowly but surely.

--- counter.py
+++ counter.py
@@ -1,5 +1,15 @@
 # Read CLock Counter, QIE Reset Counter and WTE Counter
 
+import client
+import time
+
+# Setup web bus and i2c addresses.
+pi = "192.168.1.41"
+bus = client.webBus(pi, 0)
+ccm = 0x74
+gpio = 0x70
+igloo = 0x09
+
 # Bridge and Igloo Counters
 # Clock Counter
 #   address 0x12
@@ -10,15 +20,6 @@
 # WTE Counter
 #   address 0x14
 #   size    4 bytes
-
-import client
-import time
-
-pi = "192.168.1.41"
-bus = client.webBus(pi, 0)
-ccm = 0x74
-gpio = 0x70
-igloo = 0x09
 
 # Counter register addresses
 counterDict = { "b_clock" : 0x12,
@@ -37,6 +38,8 @@ sipmBus = [0x02, 0x20, 0x01, 0x04]
 calBus = 0x10
 # QIE Card i2c address
 qieAddress = [0x19, 0x1a, 0x1b, 0x1c]
+
+#########################################################
 
 def getRmBus(rm):
     return rmBus[rm-1]
@@ -81,51 +84,40 @@ def getValue(message):
 
 #########################################################
 
-# Read counter from QIE card given RM (1-4) and Slot (1-4)
-# Reads from Bridge or Igloo
-def readCounter(rm, slot, counter):
-    # Card is i2c address, register is register address 
-    card = getQieAddress(slot)
-    register = counterDict[counter]
-    # Read Bridge Counter
-    if counter[0] == 'b':
-        bus.write(ccm, [getRmBus(rm)])
-        bus.write(card, [register])
-        bus.read(card, size)
-    # Read Igloo Counter
-    else:
-        bus.write(ccm, [getRmBus(rm)])
-        bus.write(card, [0x11,0x03,0,0,0])
-        bus.write(igloo, [register])
-        bus.read(igloo, size)
-    # Send the batch!
-    m = bus.sendBatch()
-    return m
-
+# Reset qie card bridge fpga
 def bridgeReset():
     bus.write(0x00, [0x06])
     m = bus.sendBatch()
     return m
 
-# Original value: 41
-def magicReset():
-    bus.write(ccm,[0x08]) # PCA9538 is bit 3 on ngccm mux
-    bus.read(gpio,1)
-    batch_1 = bus.sendBatch()
-    value = getValue(batch_1[-1])
-    print batch_1
-    print value
-    value | 0x18
-    #power on and reset
+# Reset gpio 
+def gpioReset():
+    # gpio reset
     #register 3 is control reg for i/o modes
+    bus.write(ccm,[0x08])
     bus.write(gpio,[0x03,0x00]) # sets all GPIO pins to 'output' mode
     bus.write(gpio,[0x01,0x00])
     bus.write(gpio,[0x01,0x08])
-    bus.write(gpio,[0x01,value]) # GPIO reset is 10
+    bus.write(gpio,[0x01,0x18]) # GPIO reset is 10
     bus.write(gpio,[0x01,0x08])
     batch = bus.sendBatch()
-    print 'Batch : '+str(batch)
+    print 'gpio reset: {0}'.format(batch)
 
+# Set gpio to output mode
+def gpioOutputMode():
+    #register 3 is control reg for i/o modes
+    bus.write(ccm,[0x08])
+    bus.write(gpio,[0x03,0x00]) # sets all GPIO pins to 'output' mode
+    batch = bus.sendBatch()
+    error_code = batch[-1][0]
+    if error_code == '1':
+        print "Set GPIO output mode fail"
+        return False
+    else:
+        print "Set GPIO output mode successful"
+        return True
+
+# Turns bits on (on=True) or off (on=False)
 def reset(bit, on=False):
     bus.write(ccm,[0x08])
     bus.read(gpio,1)
@@ -139,13 +131,13 @@ def reset(bit, on=False):
     bus.read(gpio,1)
     batch_2 = bus.sendBatch()
     value_2 = getValue(batch_2[-1])
-    #print batch_2
-    #print value_2
 
+# Use value | bit operator to turn bits on
 def setBit(value, bit):
     value = value | bit
     return value
 
+# Use value & ~bit to turn bits off
 def killBit(value, bit):
     value = value & ~bit
     return value
@@ -155,41 +147,80 @@ def powerReset():
     bus.write(gpio,[0x08,0])
     bus.sendBatch()
 
-# 8 0 (bit 10 is reset) the bit 08 is counter enable
-# reset high
-# reset low
-# counter enable high
-# wait a second
-# counter enable low
 
-def readCounters(rm, register_list):
+# Read counter from QIE card given RM (1-4) and Slot (1-4)
+# Reads from Bridge or Igloo
+def readCounter(rm, slot, counter):
+    # Card is i2c address, register is register address 
+    card = getQieAddress(slot)
+    register = counterDict[counter]
+    #bus.write(0x00,[0x06])
+    bus.write(ccm, [getRmBus(rm)])
+    # Read Bridge Counter
+    if counter[0] == 'b':
+        bus.write(card, [register])
+        bus.read(card, size)
+    # Read Igloo Counter
+    else:
+        bus.write(card, [0x11,0x03,0,0,0])
+        bus.write(igloo, [register])
+        bus.read(igloo, size)
+    # Send the batch!
+    m = bus.sendBatch()
+    return m
+
+def readCounters(rm_list, register_list):
+    outputs = []
     for c in register_list:
-        for slot in [1,2,3,4]:
-            # Read and print counter 
-            m = readCounter(rm, slot, c)
-            v = getValue(m[-1])
-            print "{0} : {1}".format(c,v)
+        for rm in rm_list:
+            for slot in [1,2,3,4]:
+                # Read and print counter 
+                m = readCounter(rm, slot, c)
+                v = getValue(m[-1])
+                outputs.append("{0}".format(v))
+                #print "{0} : {1}".format(c,v)
+    print "\t".join(outputs)
+        
+# bit = 0x10 is reset counter
+# bit = 0x08 is counter enable
+# reset high            turn bit = 0x10 on
+# reset low             turn bit = 0x10 off
+# read counters         counts = 0 after reset
+# counter enable high   turn bit = 0x08 on
+# wait a second         wait for t = 1 seconds
+# counter enable low    turn bit = 0x08 off
+# read counters         counts = 11000 after 1 second (11 kHz)
+def count(rm_list, register_list, wait, iterations=1):
+    # Set gpio output mode
+    status = gpioOutputMode()
 
-def count(rm, wait, register_list):
-    # Reset High and Low
-    reset(0x10, True)
-    reset(0x10, False)
+    # Gpio output mode succeeded
+    if status:
+        # Reset High and Low
+        reset(0x10, True)
+        reset(0x10, False)
 
-    # Read counters 
-    readCounters(rm, register_list)
+        # Read counters 
+        readCounters(rm_list, register_list)
 
-    # Counter enable high, wait, counter enable low
-    reset(0x08, True)
-    time.sleep(wait)
-    reset(0x08, False)
+        for i in xrange(iterations):
+            # Counter enable high, wait, counter enable low
+            reset(0x08, True)
+            time.sleep(wait)
+            reset(0x08, False)
 
-    # Read counters 
-    readCounters(rm, register_list)
+            # Read counters 
+            readCounters(rm_list, register_list)
+
+        reset(0x08, False)
+    else:
+        print "Check power / connections."
 
 #########################################################
 
 if __name__ == "__main__":
+    rm_list = [2]
     reg_list = ["b_reset", "i_reset", "b_wte", "i_wte"]
-    count(2, 1, reg_list)
+    count(rm_list, reg_list, 0.1, 10)
 
 

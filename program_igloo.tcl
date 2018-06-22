b6af2ddbaf63c01f119d700ff5e9de0a2c963a5c
Caleb James Smith <colorado.caleb.smith@gmail.com>
2018-06-22 20:20:15 +0100

    change pi ip address to be explicit and update tcl file for flashpro

--- program_igloo.tcl
+++ program_igloo.tcl
@@ -1,9 +1,9 @@
 # Microsemi Tcl Script
 # flashpro
 # Date: Tue Oct 11 09:37:47 2016
-# Directory C:\Users\pastika\Desktop
-# File C:\Users\pastika\Desktop\program_igloo.tcl
-open_project -project {C:/Users/pastika/Documents/ngCCM/gui-step1/HE_igloo/HE_igloo.pro} -connect_programmers 1
-set_programming_file -file {C:/Users/pastika/Documents/ngCCM/gui-step1/HE_igloo/projectData/main_HE_RM_01_0B.stp}
+# Directory C:\Users\hcaldaq\gui-step1
+# File C:\Users\hcaldaq\gui-step1\program_igloo.tcl
+open_project -project {//hcaldaq-pc/Users/hcaldaq/gui-step1/HB_igloo/HB_igloo.pro} -connect_programmers 1
+set_programming_file -file {//hcaldaq-pc/Users/hcaldaq/gui-step1}
 run_selected_actions
 close_project

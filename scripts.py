819cb82a0fe3dc25754fb6cb609d8de8d79f0298
Caleb Smith <colorado.caleb.smith@gmail.com>
2018-06-26 11:57:58 -0500

    rewrite selectGpio() to match HB GUI because HB GUI works for programming Igloos

--- scripts.py
+++ scripts.py
@@ -302,7 +302,7 @@ class Teststand:
         #self.myBus.sendBatch()
 
     # Writes proper GPIO to ngCCM Emulator for given jslot.  Used for programming cards.
-    def selectGpio(self, jslot):
+    def selectGpio_old(self, jslot):
         self.jslot = jslot
         # Defines GPIO values. Only used for reference.
         #jSlotDict = {"J2 and J18" : 0x29, "J3 and J19" : 0x89, "J4 and J20" : 0xA9,
@@ -363,6 +363,91 @@ class Teststand:
         print "Reading from BRDG_ADDR_IGLO_CONTROL after selecting JTAG: message = {0}".format(message)
         
         print "Ready to program {0} igloo".format(igloo)
+
+    # Opens the proper GPIO slot. Used for programming cards.
+    def selectGpio(self,jslot,igloo):
+        '''
+        jSlotDict = {"J2 and J18" : 0x29, "J3 and J19" : 0x89, "J4 and J20" : 0xA9,
+                     "J5 and J21" : 0x49, "J7 and J23" : 0x2A, "J8 and J24" : 0x8A,
+                     "J9 and J25" : 0xAA, "J10 and J26" : 0x4A}
+
+
+        # Full Backplane Functionality
+        newJSlotDict = {"J2 and J21" : [0x29,0x49], "J3 and J20" : [0x89,0xA9],
+                        "J4 and J19" : [0xA9,0x89], "J5 and J18" : [0x49,0x29],
+                        "J7 and J26" : [0x2A,0x4A], "J8 and J25" : [0x8A,0xAA],
+                        "J9 and J24" : [0xAA,0x8A], "J10 and J23" : [0x4A,0x2A]}
+
+        dictStringToInts = {"J2 and J21" : [2, 21], "J3 and J20" : [3, 20],
+                            "J4 and J19" : [4, 19], "J5 and J18" : [5, 18],
+                            "J7 and J26" : [7, 26], "J8 and J25" : [8, 25],
+                            "J9 and J24" : [9, 24], "J10 and J23" : [10, 23]}
+        '''
+        
+        gpioValueDict = {  2  : 0x29, 3  : 0x89, 4  : 0xA9, 5  : 0x49,
+                           7  : 0x2A, 8  : 0x8A, 9  : 0xAA, 10 : 0x4A,
+                           18 : 0x29, 19 : 0x89, 20 : 0xA9, 21 : 0x49,
+                           23 : 0x2A, 24 : 0x8A, 25 : 0xAA, 26 : 0x4A,
+                           12 : 0x2C}
+
+        self.jslot = jslot
+        gpioVal = gpioValueDict[self.jslot] 
+        # Fanout Board
+        if self.board:
+            if self.jslot in [18,19,20,21,23,24,25,26]:
+                self.myBus.write(self.fanout, [self.channels[0]])   # RM 1 and 2
+            if self.jslot in [2,3,4,5,7,8,9,10,12]:
+                self.myBus.write(self.fanout, [self.channels[1]])   # RM 3 and 4 and CU
+        #batch = self.myBus.sendBatch() # may need to uncomment
+        
+        self.myBus.write(0x74, [0x08]) # PCA9538 is bit 3 on ngccm mux
+        # myBus.write(0x70,[0x01,0x00]) # GPIO PwrEn is register 3
+        #backplane power enable and backplane reset
+        #register 3 is control reg for i/o modes
+        self.myBus.write(0x70,[0x03,0x00]) # sets all GPIO pins to 'output' mode
+        self.myBus.write(0x70,[0x01,0x08]) # GPIO value 0x08: bakplane power enable 1
+        self.myBus.write(0x70,[0x01,0x18]) # GPIO reset 0x18: backplane reset 1
+        self.myBus.write(0x70,[0x01,0x08]) # GPIO reset 0x08: backplane reset 0
+    
+        #jtag selectors finnagling for slot 26
+        self.myBus.write(0x70,[0x01,gpioVal])
+    
+        # myBus.write(0x70,[0x03,0x08])
+        self.myBus.read(0x70,1)
+        batch = self.myBus.sendBatch()
+        print "GPIO Batch = "+str(batch)
+    
+        if (batch[-1] == "1 0"):
+            print "In gpioBttnPress(): GPIO I2C_ERROR"
+            print self.I2C_ERROR_HELP
+            return
+        elif (batch[-1] == "0 "+str(gpioVal)):
+            print "GPIO " + str(newJSlotDict[self.gpioChoiceVar.get()]) + " communication SUCCESS"
+    
+        else:
+            print "GPIO Error: unexpected message is {0}".format(message)
+
+        
+        # select JTAG to program top/bottom igloo using Bridge register BRDG_ADDR_IGLO_CONTROL: 0x22
+        iglooControl = 0x22
+        message = self.readBridge(iglooControl,4)
+        print "Reading from BRDG_ADDR_IGLO_CONTROL before selecting JTAG: message = {0}".format(message)
+        value = self.getValue(message)
+        # select top (0) or bottom (1) igloo to program; maintain settings for other bits
+        if igloo == "top":
+            value = value & 0xFFE
+        elif igloo == "bot":
+            value = value | 0x001
+        else:
+            print "In selectGpio(): ERROR; Igloo is '{0}' which is not top or bot".format(igloo)
+        messageList = self.getMessageList(value,4)
+        self.writeBridge(iglooControl,messageList)
+        message = self.readBridge(iglooControl,4)
+        print "Reading from BRDG_ADDR_IGLO_CONTROL after selecting JTAG: message = {0}".format(message)
+        
+        print "Ready to program {0} igloo".format(igloo)
+
+
 
 ##################################################################################
         

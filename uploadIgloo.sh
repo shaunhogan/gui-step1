e41dbef2a620cabde609d90da4f214e2a4929713
Caleb Smith <colorado.caleb.smith@gmail.com>
2018-06-29 11:19:48 -0500

    colord removed for printout b/c windows doesn't support it

--- uploadIgloo.sh
+++ uploadIgloo.sh
@@ -7,7 +7,7 @@
 ###################################################
 #               Set Initial Data                  #
 ###################################################
-echo -e "\e[1;34mSetting initial data"
+echo "Setting initial data"
 
 # use explicit ip address because Windows machine does not resolve host aliases
 remoteHost=hep@192.168.1.11
@@ -17,28 +17,22 @@ remoteScript=$remoteDatabase/uploader/step123.sh
 jsonLoc=temp_json
 jsonTag=step3_raw.json
 
-STATUS="\e[1;34m"   # color of status statements
-ACTION="\e[1;33m"   # color of action statements
-SUCCESS="\e[1;92m"  # color of success statements
-FAIL="\e[1;91m"     # color of failure statements
-DEF="\e[39;0m"      # default colors of text
-
-echo -e "${STATUS}Initial data set"
+echo "Initial data set"
 echo ""
 
 # Note: $? is the error code of the last command executed
 # Use $? to check if the last command was successful
 
-echo -e "    ${ACTION}Copying json file to $remoteHost"
+echo "    Copying json file to $remoteHost"
 
 # 1. copy json file to Linux machine (cmshcal11)
 scp $jsonLoc/*$jsonTag $remoteHost:$remoteJson
 if [ $? -eq 0 ]
 then
-    echo -e "    ${SUCCESS}Successfully copied json file to $remoteHost"
+    echo "    Successfully copied json file to $remoteHost"
 else
-    echo -e "    ${FAIL}ERROR: Failed to copy json file to $remoteHost"
-    echo -e "${FAIL}ERROR: No information uploaded to database"
+    echo "    ERROR: Failed to copy json file to $remoteHost"
+    echo "ERROR: No information uploaded to database"
     exit 1
 fi
 # 2. upload results to database
@@ -46,15 +40,15 @@ ssh $remoteHost $remoteScript
 
 if [ $? -eq 0 ]
 then
-    echo -e "    ${SUCCESS}Successfully uploaded results to database"
+    echo "    Successfully uploaded results to database"
 else
-    echo -e "    ${FAIL}ERROR: Failed to upload results to database"
-    echo -e "${FAIL}ERROR: No information uploaded to database"
+    echo "    ERROR: Failed to upload results to database"
+    echo "ERROR: No information uploaded to database"
     exit 1
 fi
 
-echo -e "${SUCCESS}Successfully uploaded results to database"
+echo "Successfully uploaded results to database"
 
-echo -e "${STATUS}Finished${DEF}"
+echo "Finished"
 
 

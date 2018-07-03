88eae5f5b3a955ad973d14d2f7489bed161e07b9
Caleb James Smith <colorado.caleb.smith@gmail.com>
2018-07-03 11:00:16 -0500

    fix syntax to remove programmer

--- program_igloo.tcl
+++ program_igloo.tcl
@@ -5,6 +5,6 @@
 # File C:\Users\hcaldaq\gui-step1\program_igloo.tcl
 open_project -project {C:/Users/hcaldaq/gui-step1/HB_igloo/HB_igloo.pro} -connect_programmers 1
 set_programming_file -file {C:/Users/hcaldaq/gui-step1/HB_igloo/fixed_HB_RM_v1_03.stp}
-remove_prg (name {84830})*
+remove_prg -name 84830
 run_selected_actions
 close_project
